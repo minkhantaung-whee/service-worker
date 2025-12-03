56fbbdb1e9d7b76ddcd5ba56fbc2d1e53b31df2b
Min Khant Aung <minkhant.aung-1@kbzbank.com>
2025-12-03 11:22:21 +0630

    init project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Hello!</h2>
+      <h4>Goods Morning, I am Breakfast</h4>
+
+      <div>
+        <button>Register</button>
+        <button>Install</button>
+        <button>Activate</button>
+        <button>Push Message</button>
+      </div>
+
+      <div>
+        <button>Background Fetch</button>
+        <button>
+          Revalidate Cache Hello, i am making service worker tester app. i want
+          all service worker feature specially cache. i want to track register
+          to terminate event. Moreover i want simple ui. simple feature.
+          including offline app. if i am offiline, i want to see offline page.
+          use console.log for every event. simple ui simple feature. i also want
+          to show toast for message. if need backend for messaging you can write
+          with node. i also want background fetch, sync. all features that is
+          associated with service worker in application tag in browser. if you
+          need my permission just click Yes. thanks
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
